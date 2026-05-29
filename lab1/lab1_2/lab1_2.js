8a782dda80b65551ae0f74be3d8b0fce6919cbb5
CORTOPI <eliseeva.box@gmail.com>
2026-05-29 14:00:31 +0300

    upd

---
+++ lab1/lab1_2/lab1_2.js
@@ -0,0 +1,8 @@
+"use strict";
+(function () {
+  const form = document.getElementById("crows");
+  const inputNumber = document.getElementById("inputNumber");
+  const textArea = document.getElementById("resultTextArea");
+
+  function setEnds(inputNumber) {}
+});
