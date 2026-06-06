b619349285befc116ee9fbacc66b31008317fed2
CORTOPI <eliseeva.box@gmail.com>
2026-06-06 18:21:05 +0300

    update lab1_2

--- lab1/lab1_2/lab1_2.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-(function () {
-  const form = document.getElementById("crows");
-  const inputNumber = document.getElementById("inputNumber");
-  const textArea = document.getElementById("resultTextArea");
-
-  function setEnds(inputNumber) {}
-});
